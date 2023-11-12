fd007ee7fbd97fedcf28b3917d244954dd7bfed5
yelo-o <flyordig@gmail.com>
2023-11-13 00:53:23 +0900

    first commit

---
+++ src/main/java/com/example/reactdemo/payroll/Employee.java
@@ -0,0 +1,51 @@
+package com.example.reactdemo.payroll;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Getter @Setter @NoArgsConstructor
+public class Employee {
+
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
+    private String description;
+
+
+    public Employee(String firstName, String lastName, String description) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(description, employee.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/reactdemo/payroll/ReactDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactdemo.payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactDemoApplication.class, args);
+	}
+
+}
