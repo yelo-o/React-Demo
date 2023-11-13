ccc04245cb1221c09de7ccd4341a6b4532bff1e1
yelo-o <flyordig@gmail.com>
2023-11-13 10:59:46 +0900

    Employee datas are displayed in index.html

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,27 +1,34 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const client = require('./client');
+'use strict';
 
-class App extends React.Component {
+// tag::vars[]
+const React = require('react'); // <1>
+const ReactDOM = require('react-dom'); // <2>
+const client = require('./client'); // <3>
+// end::vars[]
+
+// tag::app[]
+class App extends React.Component { // <1>
 
     constructor(props) {
         super(props);
         this.state = {employees: []};
     }
 
-    componentDidMount() {
+    componentDidMount() { // <2>
         client({method: 'GET', path: '/api/employees'}).done(response => {
-        this.setState({employees: response.entity._embedded.employees});
+            this.setState({employees: response.entity._embedded.employees});
         });
     }
 
-    render() {
+    render() { // <3>
         return (
-        <EmployeeList employees={this.state.employees}/>
+            <EmployeeList employees={this.state.employees}/>
         )
     }
 }
+// end::app[]
 
+// tag::employee-list[]
 class EmployeeList extends React.Component{
     render() {
         const employees = this.props.employees.map(employee =>
@@ -41,3 +48,25 @@ class EmployeeList extends React.Component{
         )
     }
 }
+// end::employee-list[]
+
+// tag::employee[]
+class Employee extends React.Component{
+    render() {
+        return (
+            <tr>
+                <td>{this.props.employee.firstName}</td>
+                <td>{this.props.employee.lastName}</td>
+                <td>{this.props.employee.description}</td>
+            </tr>
+        )
+    }
+}
+// end::employee[]
+
+// tag::render[]
+ReactDOM.render(
+    <App />,
+    document.getElementById('react')
+)
+// end::render[]
